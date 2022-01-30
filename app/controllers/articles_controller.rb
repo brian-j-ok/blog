c542f5979a2e803899a05af213a6890dc614a424
Brian O'Kelly <brianjrok@gmail.com>
2022-01-30 13:44:09 -0500

    Fifth Commit:
    Added functionality to create a new article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,25 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    # Redirect will cause the browser to make a new request
+    # Render renders the specified view for the current request
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
